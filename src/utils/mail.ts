6a44cdb85d02a28ff10ce0c62732f792c84884f0
Set Espejo <set.espejo@sygno.com.mx>
2023-11-22 11:44:19 -0700

    changes

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -5,7 +5,7 @@ export const smtpTransport = nodemailer.createTransport({
   port: process.env.SMTP_PORT,
   secure: false,
   auth: { user: process.env.SMTP_USERNAME, pass: process.env.SMTP_PASSWORD },
-  // tls: {
-  //   rejectUnauthorized: false,
-  // },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
